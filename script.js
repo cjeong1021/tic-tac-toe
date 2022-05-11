36c574c303d673fe4ad98565ec08cf8c2a6d2daf
Christopher <cjeong1021@gmail.com>
2022-05-11 14:35:29 -0400

    add box color change on click

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+let grid = document.querySelectorAll('.box');
+let firstTurn = true;
+
+grid.forEach((box) =>{    
+    box.addEventListener('click', () => {
+        console.log('clicked');
+        console.log(box.classList);
+        if (firstTurn === true){
+            box.classList.add('blue');
+            box.classList.remove('red');
+            firstTurn = false;
+        } else if (firstTurn === false){
+            box.classList.add('red');
+            box.classList.remove('blue');
+            firstTurn = true;
+        } else {
+        }
+    })
+})
