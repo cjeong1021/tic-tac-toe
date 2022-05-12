1b4b3b7e973123aa40bfec9f03d2b9947b0ac761
Christopher <cjeong1021@gmail.com>
2022-05-12 11:48:48 -0400

    modify eventlisteners and simulate board state

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 let grid = document.querySelectorAll('.box');
 let restartButton = document.querySelector('button');
+let playerTurn = document.querySelector('h3');
 let turn = 1;
+let boardArray = [
+    '','','',
+    '','','',
+    '','',''
+]
 
-grid.forEach((box) =>{   
-    let boxClick = false;
-    box.addEventListener('click', () => {
-        if (turn % 2 === 0 && boxClick === false){
+
+restartButton.addEventListener('click', () => {
+    grid.forEach((box) =>{
+        box.classList.remove('blue', 'red');
+        turn = 1;
+        playerTurn.innerText = "Turn: Player 1";
+        boardArray = [
+            '','','',
+            '','','',
+            '','',''
+        ]
+        startGame();
+        
+    })
+})
+
+startGame();
+
+function startGame () {grid.forEach((box) =>{
+    box.addEventListener('click', function colorChange() {
+        if (turn % 2 === 0){
             box.classList.add('red');
             box.classList.remove('blue');
             turn++;
-            boxClick = true;
-        } else if (turn % 2 !== 0 && boxClick === false){
+            box.removeEventListener('click', colorChange);
+            playerTurn.innerText = "Turn: Player 1";
+            boardArray[this.id] = 'o';
+
+        } else if (turn % 2 !== 0){
             box.classList.add('blue');
             box.classList.remove('red');
             turn++;
-            boxClick = true;
+            box.removeEventListener('click', colorChange);
+            playerTurn.innerText = "Turn: Player 2";
+            boardArray[this.id] = 'x';
+
         } else {
         }
     })
 })
+}
